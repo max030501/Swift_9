aff4552feb1cc7853e2a3564530262c5c8528e35
max030511 <max030511@gmail.com>
2021-06-15 14:33:28 -0700

    Initial Commit

---
+++ CarouselApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarouselApp
+//
+//  Created by Maxim on 15.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
